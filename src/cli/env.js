748cb811f6c4069cec733692ad4472b1b16d0f16
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 19:09:26 +0500

    feat: complete env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  const res = [];
+
+  for (let key in env) {
+    if (key.startsWith('RSS_')) {
+      res.push(key + '=' + env[key])
+      // console.log(`${key}=${env[key]};`);  // если нужен вывод с новой строки
+    }
+  } 
+  console.log(res.join('; ')); // RSS_foo=bar; RSS_bar=baz
 };
 
 parseEnv();
