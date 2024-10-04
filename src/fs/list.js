e5f8aa470f3423ab38ecfea60e431c55057d61c8
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 17:32:04 +0500

    feat: complete list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.access("./files");
+  } catch {
+    throw new Error("FS operation failed: folder Files does not exists");
+  }
+
+  const files = await fs.readdir("./files");
+  console.log(files);
 };
 
 await list();
+
+// ТЗ поняла как вывести "массив с именами всех файлов в files", надеюсь правильно поняла... 
