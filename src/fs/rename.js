0c813067828be9de43e1eb6a91c8fd4a75089452
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 15:27:38 +0500

    feat: complete rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  const access = await fs.access("./files/properFilename.md")
+  .then(() => true)
+  .catch(() => false);
+
+  if (!access) {
+    try {
+      await fs.access("./files/wrongFilename.txt");
+    } catch {
+      throw new Error("FS operation failed: wrongFilename.txt does not exists");
+    }
+
+    await fs.rename('./files/wrongFilename.txt', './files/properFilename.md');
+  } else {
+    throw new Error("FS operation failed: properFilename.md already exists");
+  }
 };
 
 await rename();
