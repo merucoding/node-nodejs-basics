68553449ac6d508c053fde3c931174d2dd5f5134
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 14:39:44 +0500

    feat: complete copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access("./files");
+  } catch {
+    throw new Error("FS operation failed: folder Files does not exists");
+  }
+
+  try {
+    await fs.mkdir("./files_copy");
+  } catch {
+    throw new Error("FS operation failed: folder Files_copy already exists");
+  }
+
+  await fs.cp("./files", "./files_copy", { recursive: true });
 };
 
 await copy();
