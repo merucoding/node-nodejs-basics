7661ba555b87ea92d1f37d63b2558762fc31842b
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 17:37:00 +0500

    feat: complete read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await fs.access("./files/fileToRead.txt");
+  } catch {
+    throw new Error("FS operation failed: file does not exists");
+  }
+
+  const data = await fs.readFile("./files/fileToRead.txt", { encoding: 'utf8' });
+  console.log(data);
 };
 
 await read();
