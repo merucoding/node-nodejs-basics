16374dd0a34911092fbc5242f1bce451ddf6d7e4
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 19:56:42 +0500

    feat: complete args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const env = process.argv;
+
+  const res = env
+  .filter((el) => el.startsWith("--"))
+  .map((el) => `${el.slice(2)} is ${env[env.indexOf(el) + 1]}`)
+  .join(', ');
+
+  console.log(res); // some-arg is value1, other is 1337, arg2 is 42
 };
 
 parseArgs();
