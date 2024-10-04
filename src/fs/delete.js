168ccf5591bbf96c9e98b25c2b471ed3e333849c
Meruert <mika.amantaikuzu@gmail.com>
2024-10-04 15:48:56 +0500

    feat: complete delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fs.access("./files/fileToRemove.txt");
+  } catch {
+    throw new Error("FS operation failed: file does not exists");
+  }
+
+  await fs.rm("./files/fileToRemove.txt");
 };
 
 await remove();
