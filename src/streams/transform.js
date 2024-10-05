35106aad6c69dac261cc55a608d633eaf0cae1de
Meruert <mika.amantaikuzu@gmail.com>
2024-10-05 23:38:17 +0500

    feat: complete transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from "stream";
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    const reversedData = chunk.toString().split("").reverse().join("");
+    callback(null, reversedData);
+  },
+});
+
+const transform = () => {
+  process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
-await transform();
+transform();
