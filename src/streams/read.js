75fa3ec728aad3f5b2ac5468a270504143356b51
Meruert <mika.amantaikuzu@gmail.com>
2024-10-05 23:18:00 +0500

    feat: complete write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,11 +2,13 @@ import fs from "fs";
 import { stdout } from "process";
 
 const read = async () => {
-  const rs = fs.createReadStream('src/streams/files/fileToRead.txt', { encoding: 'utf8' });
+  const rs = fs.createReadStream("src/streams/files/fileToRead.txt", {
+    encoding: "utf8",
+  });
 
-  rs.on('data', (chunk) => {
-    stdout.write(chunk + '\n');
-  })
+  rs.on("data", (chunk) => {
+    stdout.write(chunk + "\n");
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import { stdin } from "process";
+
 const write = async () => {
-    // Write your code here 
+  const ws = fs.createWriteStream("src/streams/files/fileToWrite.txt");
+
+  stdin.on("data", (data) => {
+    ws.write(data);
+  });
+
+  stdin.on("end", () => { // ctrl + D
+    ws.end();
+    console.log("Запись завершена");
+  });
 };
 
 await write();
