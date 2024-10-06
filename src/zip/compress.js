a47c6d53028a11018b121a6b695d1dc73559230a
Meruert <mika.amantaikuzu@gmail.com>
2024-10-06 10:43:09 +0500

    feat: complete compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import zlib from "zlib";
+import { pipeline } from "stream/promises";
+
 const compress = async () => {
-    // Write your code here 
+  const rs = fs.createReadStream("src/zip/files/fileToCompress.txt");
+  const ws = fs.createWriteStream("src/zip/files/archive.gz")
+  const compressStream = zlib.createGzip();
+
+  try {
+    await pipeline(rs, compressStream, ws)
+  } catch (error) {
+    console.error('Failed: ', error);
+  }
 };
 
 await compress();
