49234e076b3478267a1d3fd85d253ec393d3dde9
Meruert <mika.amantaikuzu@gmail.com>
2024-10-06 11:23:17 +0500

    feat: complete decompress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -4,13 +4,20 @@ import { pipeline } from "stream/promises";
 
 const compress = async () => {
   const rs = fs.createReadStream("src/zip/files/fileToCompress.txt");
-  const ws = fs.createWriteStream("src/zip/files/archive.gz")
+  const ws = fs.createWriteStream("src/zip/files/archive.gz");
   const compressStream = zlib.createGzip();
 
   try {
-    await pipeline(rs, compressStream, ws)
+    await pipeline(rs, compressStream, ws);
   } catch (error) {
-    console.error('Failed: ', error);
+    console.error("Failed: ", error);
+  }
+
+  try {
+    await fs.promises.writeFile("src/zip/files/fileToCompress.txt", "", "utf8"); // очищает файл
+    console.log("File cleared");
+  } catch (error) {
+    console.error("File is not empty", error);
   }
 };
 
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import zlib from "zlib";
+import { pipeline } from "stream/promises";
+
 const decompress = async () => {
-    // Write your code here 
+  const access = await fs.promises
+    .access("src/zip/files/archive.gz")
+    .then(() => true)
+    .catch(() => false);
+
+  if (access) {
+    const rs = fs.createReadStream("src/zip/files/archive.gz");
+    const ws = fs.createWriteStream("src/zip/files/fileToCompress.txt");
+    const decompressStream = zlib.createGunzip();
+
+    await pipeline(rs, decompressStream, ws);
+  } else {
+    throw new Error("Please compress first!");
+  }
 };
 
 await decompress();
